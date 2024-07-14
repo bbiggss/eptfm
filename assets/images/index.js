59862ec52639b8288502cb4b363c5dfaeed66033
biggsky <sunghyun0624@naver.com>
2024-07-14 22:26:26 +0900

    Updates

---
+++ assets/images/index.js
@@ -0,0 +1,55 @@
+import logoImg from './logo.png';
+import bottomLogoImg from './bottom_logo.png';
+import aboutImg1 from './about_img1.png';
+import aboutImg2 from './about_img2.png';
+import aboutImg3 from './about_img3.png';
+import businessBg from './bussiness_bg.png';
+import businessImg1 from './bussiness_img1.png';
+import businessImg2 from './bussiness_img2.png';
+import businessImg3 from './bussiness_img3.png';
+import businessHoverImg1 from './bussiness_img1_over.png';
+import businessHoverImg2 from './bussiness_img2_over.png';
+import businessHoverImg3 from './bussiness_img3_over.png';
+import portfolioImg1 from './portfolio_img1.png';
+import portfolioImg2 from './portfolio_img2.png';
+import portfolioImg3 from './portfolio_img3.png';
+import portfolioBg from './portfolio_bg.png';
+import portfolioBg2 from './portfolio_bg2.png';
+import portfolioBg3 from './portfolio_bg3.png';
+import testImg from './02.jpg';
+import dropDown from './dropdown.png';
+import listButton from './btn_p_list.png';
+import backButton from './btn_p_back.png';
+
+export {
+  // 헤더
+  logoImg,
+  dropDown,
+  // 메인
+  // about
+  aboutImg1,
+  aboutImg2,
+  aboutImg3,
+  // business
+  businessBg,
+  businessImg1,
+  businessImg2,
+  businessImg3,
+  businessHoverImg1,
+  businessHoverImg2,
+  businessHoverImg3,
+  // portfolio
+  portfolioImg1,
+  portfolioImg2,
+  portfolioImg3,
+  portfolioBg,
+  portfolioBg2,
+  portfolioBg3,
+
+  // portfolio페이지
+  listButton,
+  backButton,
+  // 푸터
+  bottomLogoImg,
+  testImg,
+};
