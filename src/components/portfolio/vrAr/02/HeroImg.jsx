ad887cfc6cdf90843bf67caadcff0b71ff6dc7ca
biggsky <sunghyun0624@naver.com>
2024-08-07 18:25:24 +0900

    0807

--- src/components/portfolio/vrAr/02/HeroImg.jsx
+++ src/components/portfolio/vrAr/02/HeroImg.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
-const StyledHeroImg = styled.img``;
+const StyledHeroImg = styled.img`
+  opacity: ${(props) => (props.$isLoaded ? 1 : 0)};
+  transition: opacity 1s ease-in-out;
+`;
 
 const HeroImg = (data) => {
-  return <img src={data.src} alt="" />;
+  const [isLoaded, setIsLoaded] = useState(false);
+  const imgRef = useRef(null);
+  useEffect(() => {
+    const img = imgRef.current;
+    if (img) {
+      const handleLoad = () => {
+        setIsLoaded(true);
+      };
+      img.addEventListener('load', handleLoad);
+
+      return () => {
+        img.removeEventListener('load', handleLoad);
+      };
+    }
+  }, []);
+
+  return <StyledHeroImg ref={imgRef} src={data.src} alt="" />;
 };
 
 export default HeroImg;
