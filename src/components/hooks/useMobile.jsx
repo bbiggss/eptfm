dccec29299d70fd99e763977d23f1f4ea949c282
unknown <sunghyun0624@naver.com>
2024-08-26 15:21:49 +0900

    usemobile usetablet value change

--- src/components/hooks/useMobile.jsx
+++ src/components/hooks/useMobile.jsx
@@ -4,7 +4,8 @@ import { useMediaQuery } from 'react-responsive';
 const useMobile = () => {
   const [isMobile, setIsMobile] = useState(false);
   // const mobile = useMediaQuery({ query: '(max-width:767px)' });
-  const mobile = useMediaQuery({ query: '(max-width:480px)' });
+  // const mobile = useMediaQuery({ query: '(max-width:480px)' });
+  const mobile = useMediaQuery({ query: '(max-width:700px)' });
   useEffect(() => {
     setIsMobile(mobile);
   }, [mobile]);
